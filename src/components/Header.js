db4b248fdaaf81e9838cf516a92e1e18c23a7dbd
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-20 23:28:49 +0100

    responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = ({ logo, themeClick, dayLogo }) => {
     const navMobile = <nav className={`header__nav-mobile ${navGround}`}>
                 <div className='header__box-mobile'>
                     <img src={logo} alt='' onClick={themeClick}/>
-                    <a href='##'>{logo === dayLogo ? "Dark Mode" : "Light Mode"}</a>
+                    <a href='##' onClick={themeClick}>{logo === dayLogo ? "Dark Mode" : "Light Mode"}</a>
                 </div>
                 <ul className='header__list-mobile'>
                     <li><Link to='/' className={isActive === 1 ? 'active': ''} onClick={() => makeActive(1)}>Home</Link></li>
