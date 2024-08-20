795a880a79d369ee4c923cc6f23476f95d4462d7
jubrealguy <whizkidjubrealguy@gmail.com>
2024-08-20 22:56:02 +0100

    responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,42 @@
 import brand from '../img/Subtract.png'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import nightHam from '../img/ham-black.png'
+import dayHam from '../img/ham-light.png'
+import dayCancel from '../img/dayCancel.png'
+import nightCancel from '../img/nightCancel.png'
 
 const Header = ({ logo, themeClick, dayLogo }) => {
+    const [clickedNav, setclickedNav] = useState(true);
     const [isActive, setIsActive] = useState(1)
     const headerClass = logo === dayLogo ? 'header-night' : 'header-day';
+    const navGround = logo === dayLogo ? 'navGround-night' : 'navGround-day';
+
+    const clickHam = (e) => {
+        e.preventDefault();
+        setclickedNav(!clickedNav);
+    }
 
     const makeActive = (index) => {
         setIsActive(index)
     }
 
-    const navMobile = <nav className='header__nav-mobile'>
+    const navMobile = <nav className={`header__nav-mobile ${navGround}`}>
+                <div className='header__box-mobile'>
+                    <img src={logo} alt='' onClick={themeClick}/>
+                    <a href='##'>{logo === dayLogo ? "Dark Mode" : "Light Mode"}</a>
+                </div>
                 <ul className='header__list-mobile'>
                     <li><Link to='/' className={isActive === 1 ? 'active': ''} onClick={() => makeActive(1)}>Home</Link></li>
                     <li><Link to='/about' className={isActive === 2 ? 'active': ''} onClick={() => makeActive(2)}>About Us</Link></li>
                     <li><Link to='/services' className={isActive === 3 ? 'active': ''} onClick={() => makeActive(3)}>Services</Link></li>
                     <li><Link to='/projects' className={isActive === 4 ? 'active': ''} onClick={() =>makeActive(4)}>Our Projects</Link></li>
                 </ul>
+                <img src={logo === dayLogo ? nightCancel: dayCancel} onClick={() => setclickedNav(true)} alt='' />
             </nav>
 
     return (
+        <>
         <header className={`header ${headerClass}`}>
             <h1 className='header__primary'>
                 <img src={brand} alt='' />
@@ -37,7 +54,13 @@ const Header = ({ logo, themeClick, dayLogo }) => {
                 <img src={logo} alt='' onClick={themeClick}/>
                 <a href='##'>Contact Us</a>
             </div>
+            
+        { clickedNav && <img className='hamburger' src={logo === dayLogo ? nightHam : dayHam} onClick={clickHam} alt=''/>}
+        { !clickedNav && navMobile}
+        
         </header>
+        
+        </>
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,54 @@
 import cover from "../img/Rectangle 4489.png"
 import cardImg from '../img/card-img.png'
 
+import React, { useState, useEffect } from 'react';
+
 const Projects = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // const url = `https://cors-anywhere.herokuapp.com/https://fluxdevsng.pythonanywhere.com/api/projects`;
+                const url = "https://fluxdevsng.pythonanywhere.com/api/projects"
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                console.log(result)
+                setData(result);
+                setLoading(false);
+            } 
+            
+            catch (error) {
+                setError(error);
+                console.log(error)
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    // return (
+    //     <div className="projects-container">
+    //         {data.map((project) => (
+    //             <div key={project.id} className="project-card">
+    //                 <h3 className="project-title">{project.date}</h3>
+    //                 <p className="project-description">{project.description}</p>
+    //                 {/* Add other project details as needed */}
+    //             </div>
+    //         ))}
+    //     </div>
+    // );
+
+
     return (
         <section  className="projects">
             <h2 className="cover">Our projects</h2>
@@ -12,6 +59,22 @@ const Projects = () => {
                     <p>Embark on a visual journey as you look through our projects</p>
                 </div>
             </div>
+            <ul className="projects__list">
+                {data.map((project) => {
+                    return (
+                        <li key={project?.id} className="projects__list--item">
+                            <div>
+                                <a href="##">Click to view</a>
+                                <p className="content">{project?.name}</p>
+                                <p>{project?.description}</p>
+                                <p>{project?.more_details}</p>
+                                <p className="date">{`Project update: ${project.date}`}</p>
+                            </div>
+                            <img src={cardImg} alt="" />
+                        </li>
+                    )
+                })}
+            </ul>
             <div className="projects__recent">
                 <h2>Recent works</h2>
                 <ul className="projects__list">
